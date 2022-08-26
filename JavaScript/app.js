ed783fa2b0ff4325a550a39e1a65ae82d359c6a1
Hassan Nadeem <hn178139@gmail.com>
2022-08-26 12:21:23 +0500

    done

---
+++ JavaScript/app.js
@@ -0,0 +1,44 @@
+
+  /* var a = 2;
+console.log(a);
+document.write(a);
+*/
+  // alert(a);
+  let b = 50;
+  b = 100;
+  console.log(b);
+
+  // var b = 60;
+  console.log(b);
+  
+  {
+  let b = 2;
+  console.log(b);
+  b = 3 ;
+  
+}
+
+const age = 18 ;
+
+{
+
+const age = 20;
+console.log("age")
+
+
+
+}
+console.log(age);
+
+// let x = prompt("Enter your favourite Number " , );
+
+// let y = x + 10;
+let i = "Hassan" + 8 + 8;
+let j =  8 + 8 + "JavaScript" ;
+console.log(j)
+
+// console.log(y);
+console.log(i);
+
+
+console.log(4 + 10 +" "+ i )

a931a788a3b9881a5c32131f376caa71e36d4ed9
HassanNadeemWebD <hn178139@gmail.com>
2022-08-29 13:13:30 +0500

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,44 +1,87 @@
 
-  /* var a = 2;
-console.log(a);
-document.write(a);
-*/
-  // alert(a);
-  let b = 50;
-  b = 100;
-  console.log(b);
+// //   /* var a = 2;
+// // console.log(a);
+// // document.write(a);
+// // */
+// //   // alert(a);
+// //   let b = 50;
+// //   b = 100;
+// //   console.log(b);
 
-  // var b = 60;
-  console.log(b);
+// //   // var b = 60;
+// //   console.log(b);
   
-  {
-  let b = 2;
-  console.log(b);
-  b = 3 ;
+// //   {
+// //   let b = 2;
+// //   console.log(b);
+// //   b = 3 ;
   
-}
+// // }
 
-const age = 18 ;
+// // const age = 18 ;
 
-{
+// // {
 
-const age = 20;
-console.log("age")
-
-
-
-}
-console.log(age);
-
-// let x = prompt("Enter your favourite Number " , );
-
-// let y = x + 10;
-let i = "Hassan" + 8 + 8;
-let j =  8 + 8 + "JavaScript" ;
-console.log(j)
-
-// console.log(y);
-console.log(i);
+// // const age = 20;
+// // console.log("age")
 
 
-console.log(4 + 10 +" "+ i )
+
+// // }
+// // console.log(age);
+
+// // // let x = prompt("Enter your favourite Number " , );
+
+// // // let y = x + 10;
+// // let i = "Hassan" + 8 + 8;
+// // let j =  8 + 8 + "JavaScript" ;
+// // console.log(j)
+
+// // // console.log(y);
+// // console.log(i);
+
+
+// // console.log(4 + 10 +" "+ i )
+
+// // let abd;
+// // let abc = abd;
+// // let abc = null;
+// // console.log(typeof abc);
+
+
+// // javaScript Operators
+
+// let num1 = 2;
+// console.log(num1);
+// let num2 = 102;
+// // let num2 = prompt("Enter Your Favourite Number:", Number  ) ;
+// let ans = num2 % num1;
+//           // 102 / 2
+
+// console.log(ans);
+
+// console.log(num1 >= 10);
+// console.log("20" === 20);
+
+// num1 += 50;
+// num1 *= 15;
+// console.log(num1)
+
+let i = 1;
+console.log(i++);
+console.log( ++i )
+// i++;
+// let test = i + ++i + --i + i + i++ + ++i - --i + i++ + i;
+  //       1 +  2  + 1   + 1 +  1   + 3  -  2  +  2   + 3 
+
+  
+  let test1 =  ++i + --i + i-- + ++i + ++i + i++ + --i + i + ++i + 5 + i;
+         //      2  + 1   + 1   + 1   + 2   + 2   + 2   + 2 + 3   + 5 + 3;  
+
+
+
+console.log(test1);
+
+
+
+

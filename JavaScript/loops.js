a567b6c4794e35bdf751109f4b0c12a6b06a45f7
HassanNadeemWebD <hn178139@gmail.com>
2022-09-09 12:52:47 +0500

    commit

---
+++ JavaScript/loops.js
@@ -0,0 +1,43 @@
+// Program for generating Table of User Entered Number 
+// 2 x 1 x 2
+// let userNum = prompt("Enter Any Number")
+// for (let i = 1; i <= 10; i++) {
+
+//     console.log(userNum + " x " + i + "= " + userNum * i);
+
+
+// }
+
+let arr = ["apple", "mango", "Grapes", "Banana", "pineapple", "Tomatoes"]
+// array.forEach(element => {
+//     console.log(element)
+
+// });
+
+// for (let index = 0; index < arr.length; index++) {
+
+//     if (index != 3) {
+
+//         console.log(arr[index])
+
+
+//     }
+
+// }
+let a = 11 ;
+
+// while (a<=10) {
+//     a++
+//     console.log(a)
+//     a++
+    
+// }
+
+do {
+    
+
+
+console.log(a)
+a++
+
+} while (a<=10);

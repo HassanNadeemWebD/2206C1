1ccb38abdb0107bc819af3d3aab4ba12391dee27
Hassan Nadeem <hn178139@gmail.com>
2022-09-12 12:56:51 +0500

    update

---
+++ JavaScript/dom.js
@@ -0,0 +1,26 @@
+// console.log(document.getElementById("myheading").innerHTML = "Welcome" )
+// console.log(document.querySelector("#myheading").innerHTML)
+
+// function change(){
+
+// let tag = document.querySelector('#myheading');
+
+// tag.classList.toggle("blue")
+// document.getElementById("modal").style.display = "block"
+// document.querySelector(".btn").style.display = "none"
+
+
+// }
+// document.getElementById("myheading").style.textAlign = "center";
+// console.log(document.querySelectorAll(".mypara") )
+
+console.log(document.getElementById("name"))
+function login(){
+
+document.write(document.getElementById("name").value)
+
+
+
+}
+
+

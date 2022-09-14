67331731e95586bc14a4ba5292396bda8ea06c2b
Hassan Nadeem <hn178139@gmail.com>
2022-09-14 12:37:58 +0500

    commit

--- JavaScript/dom.js
+++ JavaScript/dom.js
@@ -14,13 +14,55 @@
 // document.getElementById("myheading").style.textAlign = "center";
 // console.log(document.querySelectorAll(".mypara") )
 
-console.log(document.getElementById("name"))
+// console.log(document.getElementById("name"))
+// function login(){
+
+// document.write(document.getElementById("name").value)
+
+
+
+// }
 function login(){
 
-document.write(document.getElementById("name").value)
+let name = document.getElementById("name").value;
+let email =document.getElementById("email").value
+let password = document.getElementById("password").value;
+document.write("Name: "+ name +"<br />" +"Email: "+ email +"<br />" +"Password: "+ password);
+localStorage.setItem("Name" , name);
+localStorage.setItem("Email" , email);
+localStorage.setItem("Password" , password )
+sessionStorage.setItem("Name" , name);
+sessionStorage.setItem("Email" , email);
+sessionStorage.setItem("Password" , password )
 
 
 
 }
+// localStorage.clear()
+
+
+// document.getElementById("user").innerHTML= localStorage.getItem("Name")
+function showPassword(){
+  let passwordField =  document.getElementById("password");
+//   console.log(passwordField)
+//   passwordField.setAttribute("type" , "text");
+if(passwordField.type === "password")
+{
+
+passwordField.type = "text"
+
+}
+else{
+
+    passwordField.type = "password"
+
+}
+
+
+
+
+
+}
+
 
 

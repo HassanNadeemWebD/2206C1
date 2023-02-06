4f995ec8aa1e42e7892c43999e49bef37917b0f2
Hassan Nadeem <64688959+HassanNadeemWebD@users.noreply.github.com>
2023-02-06 12:06:59 +0500

    commit

---
+++ PJS/dom.js
@@ -0,0 +1,16 @@
+// console.log(document.body);
+
+
+// console.log(document.getElementsByTagName('h1')[1].innerHTML);
+// console.log(document.getElementsByTagName('h1')[1].innerText );
+// document.getElementsByTagName('h1')[0].innerText = "Good Morning !"
+
+// console.log(document.getElementsByClassName('heading')[0].innerHTML);
+
+// let title = document.getElementById("title") 
+
+// console.log(title.style.backgroundColor = "yellow");
+
+// console.log(document.querySelector("#title").innerHTML);
+// console.log(document.querySelector('.heading'));
+console.log(title.innerText);
---
+++ PJS/regex.js
@@ -0,0 +1,8 @@
+let regex = /^(\+923|03)[0-4]{1}[0-9]{8}/g;
+// let contact = 
+// let testString = 42401-1483478-3;
+console.log(regex.test("+923482233981"));
+// console.log(regex.exec("+923482233981"));
+console.log(regex.toString());
+// console.log(regex.toSource());
+// console.log(regex.c);
